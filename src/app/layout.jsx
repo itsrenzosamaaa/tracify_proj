30e93da73feba7c9049bf11cb4033552ddc19d62
itsrenzosamaaa <renzoflestado512@gmail.com>
2025-02-12 15:57:49 +0800

    seo things p2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,17 +22,30 @@ export default function RootLayout({ children }) {
     <html lang="en" className={poppins.className}>
       <head>
         <title>TLC-Tracify | Login</title>
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="apple-mobile-web-app-title" content="TLC-Tracify" />        
+        <meta name="apple-mobile-web-app-title" content="TLC-Tracify" />
         <meta name="keywords" content="tlc, tracify, the lewis college" />
         <meta name="description" content="Login to access your account" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
         <meta charSet="UTF-8" />
+        <meta
+          name="google-site-verification"
+          content="google-site-verification=ACWj2uO1RWm6yPG0XOU-5hARanmlEIVjAfAq3VXPSfY"
+        />
       </head>
       <body>
         <SessionProvider>
